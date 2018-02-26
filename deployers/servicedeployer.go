0451b5f17a36a614d738ee38c8fa247d7297a36a
sadekjb <sadekj@il.ibm.com>
2018-02-26 16:14:31 +0200

    fixing missing argument

--- deployers/servicedeployer.go
+++ deployers/servicedeployer.go
@@ -1385,7 +1385,7 @@ func (deployer *ServiceDeployer) deleteAction(pkgname string, action *whisk.Acti
 
 	displayPreprocessingInfo(parsers.YAML_KEY_ACTION, action.Name, false)
 
-	if _, _, ok := deployer.Client.Actions.Get(action.Name); ok == nil {
+	if _, _, ok := deployer.Client.Actions.Get(action.Name, true); ok == nil {
 		var err error
 		var response *http.Response
 		err = retry(DEFAULT_ATTEMPTS, DEFAULT_INTERVAL, func() error {
